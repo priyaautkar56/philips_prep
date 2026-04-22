94ee94a7937e4582ed297eacd6dd2f260ce8b7a3
priyaautkar56 <priya.autkar@yahoo.com>
2026-04-22 12:18:25 -0400

    fix headless mode for CI pipeline

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=1000)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         yield page
         browser.close()
